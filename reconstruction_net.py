edcb984abee7f4bebbd88321a383f8606caf3924
tejassp2002 <62381420+tejassp2002@users.noreply.github.com>
2021-01-21 03:04:06 +0530

    Update reconstruction_net.py

--- reconstruction_net.py
+++ reconstruction_net.py
@@ -3,9 +3,7 @@ from keras.layers import Conv2DTranspose, Input
 
 def reconstruction_net(input_shape=(None,3,None,None), r):
 	input_img = Input(shape=input_shape)
-	
 	output = Conv2DTranspose(3, r, r, padding="valid", data_format="channels_first")(input_img)
-
 	model = Model(input_img, output)
 
 	return model
