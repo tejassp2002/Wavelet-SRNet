aa74c04bea837761411323b9635e36bc8c4a2eb2
tejassp2002 <62381420+tejassp2002@users.noreply.github.com>
2021-01-21 03:03:01 +0530

    Add files via upload

---
+++ reconstruction_net.py
@@ -0,0 +1,11 @@
+from keras.models import Model
+from keras.layers import Conv2DTranspose, Input
+
+def reconstruction_net(input_shape=(None,3,None,None), r):
+	input_img = Input(shape=input_shape)
+	
+	output = Conv2DTranspose(3, r, r, padding="valid", data_format="channels_first")(input_img)
+
+	model = Model(input_img, output)
+
+	return model
